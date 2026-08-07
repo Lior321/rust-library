1ea48a4459348c29e84bd89cfbe993c689fdd170
lior321 <liorperez77@gmail.com>
2026-08-07 17:38:31 +0300

    refactor the event manager into better rust

--- events/src/event_runner.rs
+++ events/src/event_runner.rs
@@ -1,87 +1,37 @@
 use crate::event::EventHandler;
-use std::collections::VecDeque;
-use std::sync::{Arc, Condvar, Mutex, Once};
 use std::thread;
 
-pub struct EventQueue<T: EventHandler> {
-    queue: Mutex<VecDeque<T>>,
-    condvar: Condvar,
+use std::sync::mpsc::{self, Receiver, Sender};
+
+pub struct EventManager<T: EventHandler + Send + 'static> {
+    sender: Sender<T>,
+    receiver: Option<Receiver<T>>,
 }
 
-impl<T: EventHandler> EventQueue<T> {
-    pub fn new() -> Arc<Self> {
-        Arc::new(Self {
-            queue: Mutex::new(VecDeque::new()),
-            condvar: Condvar::new(),
-        })
-    }
-
-    pub fn push(&self, event: T) {
-        self.queue
-            .lock()
-            .expect("Failed to lock the event queue")
-            .push_back(event);
-        self.condvar.notify_one();
-    }
-
-    pub fn pop_blocking(&self) -> Option<T> {
-        let mut queue = match self.queue.lock() {
-            Err(_) => return None,
-            Ok(event) => event,
-        };
-
-        loop {
-            if let Some(event) = queue.pop_front() {
-                return Some(event);
-            }
-            queue = self.condvar.wait(queue).unwrap();
+impl<T: EventHandler + Send + 'static> EventManager<T> {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel();
+        Self {
+            sender: sender,
+            receiver: Some(receiver),
         }
-    }
-
-    pub fn try_pop(&self) -> Option<T> {
-        match self.queue.lock() {
-            Err(_) => None,
-            Ok(mut dequeue) => dequeue.pop_front(),
-        }
-    }
-}
-
-pub struct EventManager<T: EventHandler + 'static> {
-    queue: Arc<EventQueue<T>>,
-    has_started: Once,
-}
-
-impl<T: EventHandler + 'static> EventManager<T> {
-    pub fn new() -> Arc<Self> {
-        Arc::new(Self {
-            queue: EventQueue::new(),
-            has_started: Once::new(),
-        })
     }
 
     /// Returns a handle producers can use to push events
-    pub fn queue(&self) -> Arc<EventQueue<T>> {
-        Arc::clone(&self.queue)
+    pub fn queue(&self) -> Sender<T> {
+        self.sender.clone()
     }
 
-    /// Spawns the consumer thread. Runs until the process exits. Can be called exactly once
-    pub fn start(&self) {
-        self.has_started.call_once(move || {
-            let queue = Arc::clone(&self.queue);
-            match queue.pop_blocking() {
-                None => true,
-                Some(mut event) => event.handle(),
-            };
+    /// Spawns the consumer thread. Runs until the process exits. Will start the process exectly once
+    pub fn start(&mut self) {
+        if !self.receiver.is_none() {
+            let receiver = self.receiver.take().unwrap();
 
             thread::spawn(move || {
-                loop {
-                    match queue.pop_blocking() {
-                        None => false,
-                        Some(_) => true,
-                        // Some(mut event) => event.handle(),
-                    };
+                while let Ok(mut event) = receiver.recv() {
+                    event.handle();
                 }
             });
-        })
+        }
     }
 }
--- events/src/timeout_event.rs
+++ events/src/timeout_event.rs
@@ -1,28 +1,33 @@
 use crate::event::EventHandler;
 use crate::event_runner::EventManager;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-pub fn count_once<T: EventHandler>(event_manager: Arc<EventManager<T>>, event: T, time: Duration) {
+pub fn count_once<T: EventHandler>(event_manager: &EventManager<T>, event: T, time: Duration) {
+    let queue = event_manager.queue();
     thread::spawn(move || {
         thread::sleep(time);
-        event_manager.queue().push(event);
+        queue.send(event).expect("Error: Failed to send event");
     });
 }
 
 pub fn count_on_interval<T: EventHandler + Clone>(
-    event_manager: Arc<EventManager<T>>,
+    event_manager: &EventManager<T>,
     event: T,
     initial_timeout: Duration,
     interval: Duration,
 ) {
+    let queue = event_manager.queue();
     thread::spawn(move || {
         thread::sleep(initial_timeout);
-        event_manager.queue().push(event.clone());
+        queue
+            .send(event.clone())
+            .expect("Error: Failed to send event");
         loop {
             thread::sleep(interval);
-            event_manager.queue().push(event.clone());
+            queue
+                .send(event.clone())
+                .expect("Error: Failed to send event");
         }
     });
 }
